dbd0ab490844390cfb051d57f061fe9821a00d58
ehsansam90 <65367646+ehsansam90@users.noreply.github.com>
2024-08-10 13:15:19 -0400

    static

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'main/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "main/static")]3
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]3
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','main/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
