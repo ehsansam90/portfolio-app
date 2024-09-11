3c796d0d7f51c987e9379c224ef21d5b72ad0315
ehsansam90 <65367646+ehsansam90@users.noreply.github.com>
2024-09-10 23:03:51 -0400

    debug=false

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b@s%@2d*70&8e%qb_x^&*av#)8064ay3^v(j$##g$fz5lqc*9r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','localhost','.now.sh','www.ehsansm-portfolio.com','ehsansm-portfolio.com','127.0.0.1']
 
@@ -138,9 +138,9 @@ USE_TZ = True
 STATIC_URL = 'main/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "main/static")]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
